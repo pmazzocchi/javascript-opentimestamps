58c81581bc9a1bcd5b0a48579126e44299695ac1
Paolo Mazzocchi <mazzocchip@live.it>
2019-04-29 17:47:19 +0200

    Added DGI calendar as default

--- src/calendar.js
+++ src/calendar.js
@@ -181,6 +181,7 @@ class UrlWhitelist {
 const DEFAULT_CALENDAR_WHITELIST =
     new UrlWhitelist(['https://*.calendar.opentimestamps.org', // Run by Peter Todd
     'https://*.calendar.eternitywall.com', // Run by Riccardo Casatta of Eternity Wall
+    'https://*.ots.dgi.io', // Run by Ferdinando Ametrano of Digital Gold Institute
     'https://*.calendar.catallaxy.com' // Run by Vincent Cloutier of Catallaxy
   ])
 
@@ -188,6 +189,7 @@ const DEFAULT_AGGREGATORS =
     ['https://a.pool.opentimestamps.org',
       'https://b.pool.opentimestamps.org',
       'https://a.pool.eternitywall.com',
+      'https://btc.ots.dgi.io',
       'https://ots.btc.catallaxy.com']
 
 module.exports = {
