e427885d8817f3102ae107d56caa918682537b0f
Paolo Mazzocchi <mazzocchip@live.it>
2019-05-03 11:27:08 +0200

    Fixed comma

--- src/calendar.js
+++ src/calendar.js
@@ -181,7 +181,7 @@ class UrlWhitelist {
 const DEFAULT_CALENDAR_WHITELIST =
     new UrlWhitelist(['https://*.calendar.opentimestamps.org', // Run by Peter Todd
     'https://*.calendar.eternitywall.com', // Run by Riccardo Casatta of Eternity Wall
-    'https://*.calendar.catallaxy.com' // Run by Vincent Cloutier of Catallaxy
+    'https://*.calendar.catallaxy.com', // Run by Vincent Cloutier of Catallaxy
     'https://*.ots.dgi.io', // Run by Ferdinando Ametrano of Digital Gold Institute
   ])
 
